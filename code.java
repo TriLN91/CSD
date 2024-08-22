e832d9a3a6d0a81e15b1712211f0c3373e6e9873
Le_Ngoc_Tri <nightt9104@gmail.com>
2024-08-22 10:03:58 +0700

    ok

--- code.java
+++ code.java
@@ -1,8 +1,17 @@
 
 
 public class code {
+   
+
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
+        int number = 10;
         System.out.println("hello world");
+        System.out.println("number = " + number);
+        
+
     }
-    
+
 }
