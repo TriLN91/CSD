84255dc73e8f0e2fa8cf1827b474e9f77401d626
Le_Ngoc_Tri <nightt9104@gmail.com>
2024-08-31 11:30:41 +0700

    update

--- Linked_list_buoi2/src/mylinkedlist/Node.java
+++ Linked_list_buoi2/src/mylinkedlist/Node.java
@@ -1,5 +1,16 @@
 package mylinkedlist;
 
 public class Node {
+    private int data;
+    private Node next;
+      
+    public Node() {
+    }
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
     
 }
